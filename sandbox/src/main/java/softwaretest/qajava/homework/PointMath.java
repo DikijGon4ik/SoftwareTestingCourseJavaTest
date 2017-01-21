0bd12f0ba19aa71b00841a77aa6d83341615f69a
Yonathan <dikijgon4ik@gmail.com>
2017-01-22 00:16:39 +0200

    Домашняя работа var.1

---
+++ sandbox/src/main/java/softwaretest/qajava/homework/PointMath.java
@@ -0,0 +1,16 @@
+package softwaretest.qajava.homework;
+
+public class PointMath {
+
+  public static void main(String[] args) {
+Point p1 = new Point(1,2);
+Point p2 = new Point(2,4);
+    double distance = distance(p1,p2);
+    System.out.println(distance);
+  }
+
+  public static double distance(Point p1, Point p2){
+    double res = Math.sqrt(Math.pow((p2.x-p1.x),2) + Math.pow((p2.y-p1.y),2));
+    return res;
+  }
+}
