08c1351bc6a20d4019f68ef9b134eebff03e6dcf
Yonathan <dikijgon4ik@gmail.com>
2017-01-21 21:36:39 +0200

    Добавлены математ.функции

--- sandbox/src/main/java/sotfwaretest/qajava/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/sotfwaretest/qajava/sandbox/MyFirstProgramm.java
@@ -3,6 +3,27 @@ package sotfwaretest.qajava.sandbox;
 public class MyFirstProgramm {
 
  public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+
+   System.out.println(2+2);
+   double l = 5;
+   int a = 2;
+   int b = 3;
+   System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+   System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+ }
+
+  public static void hello(String somebody){
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double l){
+    return l*l;
+  }
+
+  public  static double area(double a, double b){
+    return a*b;
   }
 }
+
+
