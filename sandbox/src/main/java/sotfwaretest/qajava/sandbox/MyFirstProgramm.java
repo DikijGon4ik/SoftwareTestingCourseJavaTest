962a52558ebf95559e31b20d600c018eacf7603a
Yonathan <dikijgon4ik@gmail.com>
2017-01-21 17:36:34 +0200

    Проект отвязан от репозитория Git

--- sandbox/src/main/java/sotfwaretest/qajava/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/sotfwaretest/qajava/sandbox/MyFirstProgramm.java
@@ -1,8 +1,8 @@
 package sotfwaretest.qajava.sandbox;
 
-public class MyFirstProgramm{
+public class MyFirstProgramm {
 
-public static void main(String[]args) {
-System.out.println("Hello, world!");
-}
+ public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
 }
