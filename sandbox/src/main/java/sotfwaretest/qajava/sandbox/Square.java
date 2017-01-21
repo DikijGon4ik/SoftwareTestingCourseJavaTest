27bffe2caec3d226c8bfafa1e7e2be8774d26493
Yonathan <dikijgon4ik@gmail.com>
2017-01-22 00:15:54 +0200

    создание объектов

---
+++ sandbox/src/main/java/sotfwaretest/qajava/sandbox/Square.java
@@ -0,0 +1,14 @@
+package sotfwaretest.qajava.sandbox;
+
+public class Square {
+  public double l;
+
+
+  public Square(double l){
+this.l = l;
+  }
+
+  public double area(){
+    return this.l*this.l;
+  }
+}
