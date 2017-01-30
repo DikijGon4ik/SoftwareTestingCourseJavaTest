23f69f7d3502e8b53df8f75a2b15731a32c59a20
Yonathan <dikijgon4ik@gmail.com>
2017-01-30 11:57:32 +0200

    Перенос логина в @BeforeMethod

--- addressbook-web-test/src/test/java/sotfware/qajava/addressbook/GroupCreationTests.java
+++ addressbook-web-test/src/test/java/sotfware/qajava/addressbook/GroupCreationTests.java
@@ -4,7 +4,6 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
-
 import java.util.concurrent.TimeUnit;
 import java.util.Date;
 import java.io.File;
@@ -19,21 +18,21 @@ public class GroupCreationTests {
     
     @BeforeMethod
     public void setUp() throws Exception {
-        System.setProperty("webdriver.gecko.driver", "F:/Studing/geckodriver.exe");
-        wd = new FirefoxDriver();
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+      System.setProperty("webdriver.gecko.driver", "F:/Studing/geckodriver.exe");
+      wd = new FirefoxDriver();
+      wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+      wd.get("http://localhost/addressbook/");
+      wd.findElement(By.name("pass")).click();
+      wd.findElement(By.name("pass")).clear();
+      wd.findElement(By.name("pass")).sendKeys("secret");
+      wd.findElement(By.name("user")).click();
+      wd.findElement(By.name("user")).clear();
+      wd.findElement(By.name("user")).sendKeys("admin");
+      wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
     
     @Test
-    public void GroupCreationTests() {
-        wd.get("http://localhost/addressbook/");
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    public void testGroupCreation() {
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
